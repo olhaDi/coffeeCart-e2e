f6bd9518160b15e94f6efed3854e3059495c1511
olhaDi <diachenkoolha@gmail.com>
2026-09-15 13:26:32 +0300

    Add tests

---
+++ tests/happyFlowTest.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('User journey', async ({ page }) => {
+  await page.goto('https://coffee-cart.app/');
+  await page.locator('[data-test="Espresso"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.getByRole('textbox', { name: 'Name' }).fill('Olia');
+  await page.getByRole('textbox', { name: 'Name' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Email' }).fill('olia@ol.ua');
+  await page.getByRole('checkbox', { name: 'Promotion checkbox' }).check();
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.getByRole('button', { name: 'Thanks for your purchase.' })).toBeVisible();
+});
