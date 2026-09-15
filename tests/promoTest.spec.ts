0f85d97c676688c0484b94bd532b606669fe2beb
olhaDi <diachenkoolha@gmail.com>
2026-09-15 14:17:54 +0300

    Improve test logic

--- tests/promoTest.spec.ts
+++ tests/promoTest.spec.ts
@@ -1,9 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('Promo offer is displayed after adding three products', async ({ page }) => {
+test('Discounted Mocha is added after accepting the promo offer', async ({ page }) => {
   await page.goto('https://coffee-cart.app/');
   await page.locator('[data-test="Espresso"]').click();
   await page.locator('[data-test="Espresso_Macchiato"]').click();
   await page.locator('[data-test="Cappuccino"]').click();
-  await expect(page.getByText('It\'s your lucky day! Get an extra cup of Mocha for $4.espressochocolate')).toContainText('an extra cup');
+  await page.getByText('It\'s your lucky day! Get an').click();
+  await expect(page.getByText('It\'s your lucky day! Get an')).toBeVisible();
+  await page.getByRole('button', { name: 'Yes, of course!' }).click();
+  await page.getByRole('link', { name: 'Cart page' }).click();
+  await expect(page.getByText('(Discounted) Mocha$4.00 x 1')).toBeVisible();
 });
