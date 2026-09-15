f4b21d4696dfcb4f3b84e2c269d628f1df40a3cf
olhaDi <diachenkoolha@gmail.com>
2026-09-15 13:54:30 +0300

    Add meaningful name to the test

--- tests/happyFlowTest.spec.ts
+++ tests/happyFlowTest.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('User journey', async ({ page }) => {
+test('User can complete a coffee order successfully', async ({ page }) => {
   await page.goto('https://coffee-cart.app/');
   await page.locator('[data-test="Espresso"]').click();
   await page.locator('[data-test="checkout"]').click();
