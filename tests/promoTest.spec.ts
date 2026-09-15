ec9d79abfab430013915437ae4f4bc38029afafb
olhaDi <diachenkoolha@gmail.com>
2026-09-15 14:01:49 +0300

    Add assertion to the test

--- tests/promoTest.spec.ts
+++ tests/promoTest.spec.ts
@@ -5,9 +5,5 @@ test('Promo code applies a discount to the order', async ({ page }) => {
   await page.locator('[data-test="Espresso"]').click();
   await page.locator('[data-test="Espresso_Macchiato"]').click();
   await page.locator('[data-test="Cappuccino"]').click();
-  await expect(page.getByText('It\'s your lucky day! Get an')).toBeVisible();
-  await page.locator('div').nth(3).click();
-  await expect(page.locator('div').nth(3)).toBeVisible();
-  await expect(page.getByRole('button', { name: 'Yes, of course!' })).toBeVisible();
-  await expect(page.getByRole('button', { name: 'Nah, I\'ll skip.' })).toBeVisible();
+  await expect(page.getByText('It\'s your lucky day! Get an extra cup of Mocha for $4.espressochocolate')).toContainText('an extra cup');
 });
